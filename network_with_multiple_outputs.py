a6cbf5080026291c5b8b47cf63cda59b31c0138f
VarMoon <49207914+VarM00n@users.noreply.github.com>
2021-03-03 17:57:01 +0100

    v0.2 complete network, wrong training

--- network_with_multiple_outputs.py
+++ network_with_multiple_outputs.py
@@ -15,79 +15,123 @@ class Perceptron:
     def sigmoid_derivative(x):
         return x * (1-x)
 
-    training_inputs = [[1, 0, 1, 0, 1, 0, 1, 0, 1],
-                       [0, 1, 0, 1, 0, 1, 0, 1, 0],
-                       [0, 0, 0, 1, 1, 1, 0, 0, 0],
-                       [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    amount_of_inputs = 4
+    amount_of_neurons_in_hl = 3
+    amount_of_outputs = 2
 
-                       [1, 0, 1, 0, 1, 0, 1, 0, 1],
-                       [1, 0, 1, 0, 1, 0, 1, 0, 1],
-                       [1, 0, 1, 0, 1, 0, 1, 0, 1],
-                       [1, 0, 1, 0, 1, 0, 1, 0, 1],
-                       [1, 0, 1, 0, 1, 0, 1, 0, 1],
-                       [1, 0, 1, 0, 1, 0, 1, 0, 1]]
+    training_inputs = [[0, 1, 1, 1],
+                       [0, 1, 0, 1],
+                       [0, 0, 0, 1],
+                       [0, 1, 0, 0],
+                       [1, 1, 1, 0],
+                       [1, 0, 1, 0],
+                       [0, 1, 1, 0],
+                       [1, 0, 1, 0],
+                       [1, 0, 1, 0],
+                       [1, 0, 1, 0]]
 
-    training_outputs = [[1, 0, 0, 0],
-                        [0, 1, 0, 0],
-                        [0, 0, 1, 0],
-                        [0, 0, 0, 1],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0],
-                        [0, 0, 0, 0]]
+    training_outputs = [[1, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0],
+                        [0, 1],
+                        [0, 0],
+                        [1, 1],
+                        [0, 0],
+                        [0, 0],
+                        [0, 0]]
 
-    input_weights = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-                     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-                     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-                     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+    input_weights = [0.5, 0.5, 0.5, 0.5,
+                     0.5, 0.5, 0.5, 0.5,
+                     0.5, 0.5, 0.5, 0.5]
 
-    hidden_layer_weights = [0.5, 0.5, 0.5, 0.5,
-                            0.5, 0.5, 0.5, 0.5,
-                            0.5, 0.5, 0.5, 0.5,
-                            0.5, 0.5, 0.5, 0.5]
+    hidden_layer_weights = [0.5, 0.5, 0.5,
+                            0.5, 0.5, 0.5]
 
 
 perceptron = Perceptron()
-for i in range(0, 1000):
-    for j in range(0, len(perceptron.training_inputs)):
-        # using algorithm presented on this site http://home.agh.edu.pl/~vlsi/AI/backp_t_en/backprop.html
-        # forward
-        hidden_layer_values = [0, 0, 0, 0]
-        output_layer_values = [0, 0, 0, 0]
-        training_input = perceptron.training_inputs[j]
-        for k in range(0, len(hidden_layer_values)):
-            sum_to_check = 0
-            for m in range(0, len(training_input)):
-                sum_to_check += training_input[m] * perceptron.input_weights[k * 9 + m]
-            hidden_layer_values[k] = perceptron.sigmoid_function(sum_to_check)
 
-        for k in range(0, len(perceptron.training_outputs[0])):
-            sum_to_check = 0
-            for m in range(0, len(output_layer_values)):
-                sum_to_check += hidden_layer_values[m] * perceptron.hidden_layer_weights[k * 4 + m]
-            output_layer_values[k] = perceptron.sigmoid_function(sum_to_check)
+for z in range(0, 10000):
+    for i in range(0, 10):
+        hidden_layer_values = []
+        output_layer_values = []
+        sigma_for_output = []
+        sigma_for_hl =[]
+        # creating y's and sigmas
+        for nhf in range(0, perceptron.amount_of_neurons_in_hl):
+            hidden_layer_values.append(0)
+            sigma_for_hl.append(0)
+        for nol in range(0, perceptron.amount_of_outputs):
+            output_layer_values.append(0)
+            sigma_for_output.append(0)
 
-        # backward
-        sigma2 = [0, 0, 0, 0]
-        sigma = [0, 0, 0, 0]
-        for k in range(0, len(output_layer_values)):
-            sigma[k] = perceptron.training_outputs[j][k] - output_layer_values[k]
-        for k in range(0, len(output_layer_values)):
-            for m in range(0, len(output_layer_values)):
-                sigma2[k] += sigma[m] * perceptron.hidden_layer_weights[k+m*len(output_layer_values)]
+        # forward propagate (input - hidden layer)
+        for m in range(0, perceptron.amount_of_neurons_in_hl):
+            sum_of_multiplication = 0
+            for k in range(0, perceptron.amount_of_inputs):
+                sum_of_multiplication += perceptron.training_inputs[i][k] * \
+                                         perceptron.input_weights[k + m * perceptron.amount_of_inputs]
+            hidden_layer_values[m] = perceptron.sigmoid_function(sum_of_multiplication)
 
-        # forward
-        for k in range(0, len(output_layer_values)):
-            for m in range(0, len(training_input)):
-                perceptron.input_weights[m + k * len(training_input)] = \
-                    perceptron.input_weights[m + k * len(training_input)] + sigma2[k] * \
-                    perceptron.sigmoid_derivative(hidden_layer_values[k]) * hidden_layer_values[k]
-        for k in range(0, len(output_layer_values)):
-            for m in range(0, len(output_layer_values)):
-                perceptron.hidden_layer_weights[m + k * len(output_layer_values)] = \
-                    perceptron.hidden_layer_weights[m + k * len(output_layer_values)] + sigma[k] * \
-                    perceptron.sigmoid_derivative(output_layer_values[k]) * output_layer_values[k]
+        # forward propagate (hidden layer - output)
+        for m in range(0, perceptron.amount_of_outputs):
+            sum_of_multiplication = 0
+            for k in range(0, perceptron.amount_of_neurons_in_hl):
+                sum_of_multiplication += hidden_layer_values[k] * \
+                                         perceptron.hidden_layer_weights[k + m * perceptron.amount_of_neurons_in_hl]
+            output_layer_values[m] = perceptron.sigmoid_function(sum_of_multiplication)
 
-print("yas")
+        # end of forward propagation
+
+        # start of backpropagation
+        # output
+        for k in range(perceptron.amount_of_outputs):
+            sigma_for_output[k] = perceptron.training_outputs[i][k] - output_layer_values[k]
+
+        # hidden layer
+        for k in range(perceptron.amount_of_neurons_in_hl):
+            sum_for_hl = 0
+            for m in range(perceptron.amount_of_outputs):
+                sum_for_hl += output_layer_values[m] * \
+                              perceptron.hidden_layer_weights[k + m * perceptron.amount_of_neurons_in_hl]
+            sigma_for_hl[k] = sum_for_hl
+
+        # forward again
+        # hidden layer
+        for k in range(perceptron.amount_of_neurons_in_hl):
+            for m in range(perceptron.amount_of_inputs):
+                perceptron.input_weights[m + k * perceptron.amount_of_inputs] += \
+                    sigma_for_hl[k] * perceptron.sigmoid_derivative(hidden_layer_values[k]) * \
+                    perceptron.training_inputs[i][m]
+        # output layer
+        for k in range(perceptron.amount_of_outputs):
+            for m in range(perceptron.amount_of_neurons_in_hl):
+                perceptron.hidden_layer_weights[m + k * perceptron.amount_of_neurons_in_hl] += \
+                    sigma_for_output[k] * perceptron.sigmoid_derivative(output_layer_values[k]) * hidden_layer_values[k]
+
+print("Network trained")
+
+first_input = input()
+second_input = input()
+third_input = input()
+fourth_input = input()
+user_input = [first_input, second_input, third_input, fourth_input]
+hidden_layer_values = [0, 0, 0]
+output_layer_values = [0, 0]
+
+for m in range(0, perceptron.amount_of_neurons_in_hl):
+    sum_of_multiplication = 0
+    for k in range(0, perceptron.amount_of_inputs):
+        sum_of_multiplication += float(user_input[k]) * \
+                                 perceptron.input_weights[k + m * perceptron.amount_of_inputs]
+    hidden_layer_values[m] = perceptron.sigmoid_function(sum_of_multiplication)
+
+# forward propagate (hidden layer - output)
+for m in range(0, perceptron.amount_of_outputs):
+    sum_of_multiplication = 0
+    for k in range(0, perceptron.amount_of_neurons_in_hl):
+        sum_of_multiplication += hidden_layer_values[k] * \
+                                 perceptron.hidden_layer_weights[k + m * perceptron.amount_of_neurons_in_hl]
+    output_layer_values[m] = perceptron.sigmoid_function(sum_of_multiplication)
+
+print(output_layer_values)
